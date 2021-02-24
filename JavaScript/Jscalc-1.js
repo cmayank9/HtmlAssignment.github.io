809e3f7923c506df973faf89ef628eafa1beb01d
Mayank Chaturvedi <cmayank208@gmail.com>
2021-02-24 20:30:50 +0530

    Update Jscalc-1.js

--- JavaScript/Jscalc-1.js
+++ JavaScript/Jscalc-1.js
@@ -23,8 +23,8 @@ function divideBy()
 
 function Add() 
 { 
-        let num1 = document.getElementById("firstNumber").value;
-        let num2 = document.getElementById("secondNumber").value;
+        let num1 = Number(document.getElementById("firstNumber").value);
+        let num2 = Number(document.getElementById("secondNumber").value);
 document.getElementById("result").innerHTML = num1 + num2;
 }
 
